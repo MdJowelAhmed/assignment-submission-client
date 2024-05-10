f408d660a5e41fc0c65d0adf19d57dc94242b517
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-05-10 20:10:46 +0600

    added create assaignment page

--- src/pages/assaignments/CreateAssignments/CreateAssignments.jsx
+++ src/pages/assaignments/CreateAssignments/CreateAssignments.jsx
@@ -1,17 +1,65 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateAssignments = () => {
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <div>
             <h2>create assignments</h2>
-            <div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Title</span>
-                    </label>
-                    <input type="text" name="title" placeholder="Title" className="input input-bordered" required />
+            <form>
+                {/* title and description */}
+                <div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Title</span>
+                        </label>
+                        <input type="text" name="title" placeholder="Title" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <input type="text" name="description" placeholder="Description" className="input input-bordered" required />
+                    </div>
                 </div>
-            </div>
+                {/* marks and thumbnail */}
+                <div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Marks</span>
+                        </label>
+                        <input type="text" name="marks" placeholder="Marks" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Thumbnail_URL</span>
+                        </label>
+                        <input type="text" name="description" placeholder="Thumbnail_URL" className="input input-bordered" required />
+                    </div>
+                </div>
+                {/* marks and thumbnail */}
+                <div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"> assignment difficulty level</span>
+                        </label>
+                        <select className=" p-3 border border-blue-200 rounded-md" name="" id="">
+                            <option value="easy">easy</option>
+                            <option value="medium">medium</option>
+                            <option value="hard">hard</option>
+                        </select>
+                        {/* <input type="text" name="marks" placeholder="Marks" className="input input-bordered" required /> */}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Thumbnail_URL</span>
+                        </label>
+                        <DatePicker className=" p-3 border border-blue-200 rounded-md w-full"  selected={startDate} onChange={(date) => setStartDate(date)} />
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
